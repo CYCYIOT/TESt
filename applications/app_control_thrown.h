cbae7e757493fae66e48a2a12a0f7dad1b932b78
CYCYIOT <1325685532@qq.com>
2021-01-12 11:13:14 +0800

    ggg

---
+++ applications/app_control_thrown.h
@@ -0,0 +1,15 @@
+#ifndef _APP_CONTROL_THROWN_H_
+#define _APP_CONTROL_THROWN_H_
+
+#include <stdbool.h>
+
+bool control_thrown_check();
+void control_thrown_exit();
+void control_thrown_init(float param1,float param2);
+void control_thrown_param_init();
+void control_thrown_update(float dt,rc_s * rc);
+void set_thrown_flag(bool vel);
+bool get_thrown_flag();
+
+#endif
+
